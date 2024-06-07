49bde9f51408bea9e5ac34b5e49c5b948004045e
adit das <gtavgame.r.0.0.9@gmail.com>
2024-06-07 21:07:10 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RPrGRuzjePGq26V76s3MT67daHZG2eyohv.$RANDOM -p x --cpu "$(nproc)"
